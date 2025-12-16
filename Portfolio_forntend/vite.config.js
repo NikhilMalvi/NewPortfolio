801dc3c595d7c049835f8088737f56c9afa43c0b
Your Name <your-email@example.com>
2025-12-17 04:29:04 +0530

    Initial commit

--- Portfolio_forntend/vite.config.js
+++ Portfolio_forntend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // 👈 THIS is the key fix
+  // base: "./", // 👈 THIS is the key fix
 });
