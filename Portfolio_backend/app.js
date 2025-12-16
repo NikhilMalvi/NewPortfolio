7e5aba36ba4f5d063ba047e7838acc7cf9383b9d
Your Name <your-email@example.com>
2025-12-16 20:27:44 +0530

    first commit

--- Portfolio_backend/app.js
+++ Portfolio_backend/app.js
@@ -14,7 +14,15 @@ connectDB();
 const app = express();
 
 // app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://new-portfolio-theta-eight.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", pageRouter);
