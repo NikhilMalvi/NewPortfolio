d217afc02e7cb737c29e0dc19d3c90b58882bb65
Your Name <your-email@example.com>
2025-12-17 07:28:36 +0530

    Initial commit

--- Portfolio_forntend/src/context/AppContext.jsx
+++ Portfolio_forntend/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import React, {
   useMemo,
   useState,
 } from "react";
-import axios from "../../utils/axiosInstance";
+import axios from "../utils/axiosInstance";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
