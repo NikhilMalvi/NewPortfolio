b32b4e69d5f764ea4da9041de45a501f24a81764
Your Name <your-email@example.com>
2026-03-27 13:41:29 +0530

    render commit

--- Portfolio_backend/app.js
+++ Portfolio_backend/app.js
@@ -38,12 +38,7 @@ app.get("/api/health", (req, res) => {
 
 app.get("/", (req, res) => res.send("api is working"));
 
-app.use(
-  cors({
-    origin: ["process.env.FRONTEND_URL", "http://localhost:5173"],
-    credentials: true,
-  }),
-);
+app.use(cors());
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
